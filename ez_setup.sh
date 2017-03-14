49c5569a22c52882a84551cf49fa11d5b169bee2
Garrett Swann <garrettswann3049@yahoo.com>
2017-03-14 00:53:45 -0400

    Added pathogen && vim plugin install to ez_setup

--- ez_setup.sh
+++ ez_setup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+#Install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#Install vim.Nerdtree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+#Install vim.UndoTree
+git clone https://github.com/mbbill/undotree.git ~/.vim/bundle/undotree
+
+#Copy my dotfiles over.
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
