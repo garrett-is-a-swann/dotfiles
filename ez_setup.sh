c80c7531c7c22d79157c0cfef52d9602ab828971
Garrett Swann <gswann@mail.csuchico.edu>
2017-03-18 18:33:17 -0700

    Added installation of supertab to ez_setup script.

--- ez_setup.sh
+++ ez_setup.sh
@@ -5,9 +5,15 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 #Install vim.Nerdtree
+    # File System navigation in vim
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
+#Install vim.supertab 
+    # Tab completion for vim.
+git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/nerdtree
+
 #Install vim.UndoTree
+    # UI for Undo Tree (as in 'control-z' kind of undo)
 git clone https://github.com/mbbill/undotree.git ~/.vim/bundle/undotree
 
 #Copy my dotfiles over.
