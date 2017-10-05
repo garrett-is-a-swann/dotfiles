93a8bd9b5e1b826e7a6f5f74ad5d7b280ef1b3cd
Garrett Swann <garrettswann3049@yahoo.com>
2017-10-04 23:17:47 -0700

    Added a bash .profile dotfile.

--- ez_setup.sh
+++ ez_setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Install Pathogen
+sudo apt-get install curl
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
@@ -16,7 +17,12 @@ git clone https://github.com/ervandew/supertab.git ~/.vim/bundle/supertab
     # UI for Undo Tree (as in 'control-z' kind of undo)
 git clone https://github.com/mbbill/undotree.git ~/.vim/bundle/undotree
 
+#Install vim.Tagbar
+    # Depends on http://ctags.sourceforge.net/
+git clone https://github.com/majutsushi/tagbar.git ~/.vim/bundle/tagbar
+
 #Copy my dotfiles over.
 cp .vimrc ~/.vimrc
 cp .tmux.conf ~/.tmux.conf
 cp .inputrc ~/.inputrc
+cp .profile ~/.profile
--- update.sh
+++ update.sh
@@ -3,6 +3,11 @@
 cp ~/.vimrc .
 cp ~/.tmux.conf .
 cp ~/.inputrc .
+cp ~/.profile .
+git add .profile
 git add .vimrc
 git add .tmux.conf
 git add .inputrc
+
+git add ez_setup.sh
+git add update.sh
